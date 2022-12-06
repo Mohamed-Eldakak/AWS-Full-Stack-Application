44099782c56b4e05d178165f84fa8419666f319b
Mohamed-Eldakak <106033884+Mohamed-Eldakak@users.noreply.github.com>
2022-12-06 21:01:47 +0200

    Update environment.prod.ts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "https://udagram-api-dev3.eba-wyg9zbd8.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev3.eba-wyg9zbd8.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
